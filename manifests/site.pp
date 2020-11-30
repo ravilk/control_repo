7e84d8ca4c791436bb212cadfde85c28e808a492
ravilk <ravilkhalilov@gmail.com>
2020-11-30 22:58:08 +0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,2 @@
 node default {
- file {'/root/README':
-   ensure  => file,
-   content => 'This is Ravil first puppet file',
-   owner   => 'root', 
-      }
- file {'/root/README':
-   owner => 'root'
-      }
 } 
